d12a241bf109a968af2e938323a84b97c695eff8
Andy Maskiell <maskiella@gmail.com>
2017-10-09 22:02:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(`${string}`).toUpperCase()
+  string = 'Hello!'
+  console.log(string).toUpperCase()
 }
 
 function logWhisper(string) {
